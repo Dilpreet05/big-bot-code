ca175e85c03740aa8e48f865fd7e81ff62e27d8d
Dilpreet <67494637+Dilpreet05@users.noreply.github.com>
2024-01-17 19:19:48 -0500

    aaa

--- src/SubsystemFiles/piston.cpp
+++ src/SubsystemFiles/piston.cpp
@@ -3,13 +3,13 @@
 bool isRightWingExtended = false;
 bool waitingOnReleaseB = false;
 
-bool isLeftWingExtended = false;
+bool isHangExtended = false;
 bool waitingOnReleaseDOWN = false;
 
 bool isGrabberExtended = false;
 bool waitingOnReleaseR2 = false;
 
-pros::ADIDigitalOut WingPistonLeft('C');
+pros::ADIDigitalOut hangPiston('C');
 pros::ADIDigitalOut WingPistonRight('D');
 pros::ADIDigitalOut GrabberPiston1('A');
 pros:: ADIDigitalOut GrabberPiston2('B');
@@ -21,58 +21,58 @@ void UpdatePistons(){
     ////////////////////////////
     //LEFT WING PISTON CONTROLS
     ////////////////////////////
-    if((master.get_digital(pros::E_CONTROLLER_DIGITAL_DOWN) == 1)&& (isLeftWingExtended == false) && (waitingOnReleaseDOWN == false) ) {
-       WingPistonLeft.set_value(true);  // Deploy the piston
-       isLeftWingExtended = true;   // Indicate it's set
+    if((master.get_digital(pros::E_CONTROLLER_DIGITAL_DOWN) == 1)&& (isHangExtended == false) && (waitingOnReleaseDOWN == false) ) {
+       hangPiston.set_value(true);  // Deploy the piston
+       isHangExtended = true;   // Indicate it's set
        waitingOnReleaseDOWN = true;  // Flag to wait for button release
      }
-     else if( (master.get_digital(pros::E_CONTROLLER_DIGITAL_DOWN) == 1) && (isLeftWingExtended == true) && (waitingOnReleaseDOWN == false) ) {
-       WingPistonLeft.set_value(false);  // Retract the piston
-       isLeftWingExtended = false;  // Indicate it's not set
+     else if( (master.get_digital(pros::E_CONTROLLER_DIGITAL_DOWN) == 1) && (isHangExtended == true) && (waitingOnReleaseDOWN == false) ) {
+       hangPiston.set_value(false);  // Retract the piston
+       isHangExtended = false;  // Indicate it's not set
        waitingOnReleaseDOWN = true;  // Flag to wait for button release
      }
      else if((master.get_digital(pros::E_CONTROLLER_DIGITAL_DOWN) == 0 )) {
        waitingOnReleaseDOWN = false;  // Flag that button has been released, so next press will toggle to the opposite state
      }
 
-    ////////////////////////////
-    //RIGHT WING PISTON CONTROLS
-    ////////////////////////////
+    // ////////////////////////////
+    // //RIGHT WING PISTON CONTROLS
+    // ////////////////////////////
 
-    if((master.get_digital(pros::E_CONTROLLER_DIGITAL_B) == 1)&& (isRightWingExtended == false) && (waitingOnReleaseB == false) ) {
-       WingPistonRight.set_value(true);  // Deploy the piston
-       isRightWingExtended = true;   // Indicate it's set
-       waitingOnReleaseB = true;  // Flag to wait for button release
-     }
-     else if( (master.get_digital(pros::E_CONTROLLER_DIGITAL_B) == 1) && (isRightWingExtended == true) && (waitingOnReleaseB == false) ) {
-       WingPistonRight.set_value(false);  // Retract the piston
-       isRightWingExtended = false;  // Indicate it's not set
-       waitingOnReleaseB = true;  // Flag to wait for button release
-     }
-     else if((master.get_digital(pros::E_CONTROLLER_DIGITAL_B) == 0 )) {
-       waitingOnReleaseB = false;  // Flag that button has been released, so next press will toggle to the opposite state
-     }
+    // if((master.get_digital(pros::E_CONTROLLER_DIGITAL_B) == 1)&& (isRightWingExtended == false) && (waitingOnReleaseB == false) ) {
+    //    WingPistonRight.set_value(true);  // Deploy the piston
+    //    isRightWingExtended = true;   // Indicate it's set
+    //    waitingOnReleaseB = true;  // Flag to wait for button release
+    //  }
+    //  else if( (master.get_digital(pros::E_CONTROLLER_DIGITAL_B) == 1) && (isRightWingExtended == true) && (waitingOnReleaseB == false) ) {
+    //    WingPistonRight.set_value(false);  // Retract the piston
+    //    isRightWingExtended = false;  // Indicate it's not set
+    //    waitingOnReleaseB = true;  // Flag to wait for button release
+    //  }
+    //  e\lse if((master.get_digital(pros::E_CONTROLLER_DIGITAL_B) == 0 )) {
+    //    waitingOnReleaseB = false;  // Flag that button has been released, so next press will toggle to the opposite state
+    //  }
 
 
-    ////////////////////////////
-    //GRABBER PISTON CONTROLS
-    ////////////////////////////
+    // ////////////////////////////
+    // //GRABBER PISTON CONTROLS
+    // ////////////////////////////
 
-    if((master.get_digital(pros::E_CONTROLLER_DIGITAL_R2) == 1)&& (isGrabberExtended == false) && (waitingOnReleaseR2 == false) ) {
-       GrabberPiston1.set_value(true);  // Deploy the piston
-       GrabberPiston2.set_value(true);
-       isGrabberExtended = true;   // Indicate it's set
-       waitingOnReleaseR2 = true;  // Flag to wait for button release
-     }
-     else if( (master.get_digital(pros::E_CONTROLLER_DIGITAL_R2) == 1) && (isGrabberExtended == true) && (waitingOnReleaseR2 == false) ) {
-       GrabberPiston1.set_value(false);  // Retract the piston
-       GrabberPiston2.set_value(false);
-       isGrabberExtended = false;  // Indicate it's not set
-       waitingOnReleaseR2 = true;  // Flag to wait for button release
-     }
-     else if((master.get_digital(pros::E_CONTROLLER_DIGITAL_R2) == 0 )) {
-       waitingOnReleaseR2 = false;  // Flag that button has been released, so next press will toggle to the opposite state
-     }
+    // if((master.get_digital(pros::E_CONTROLLER_DIGITAL_R2) == 1)&& (isGrabberExtended == false) && (waitingOnReleaseR2 == false) ) {
+    //    GrabberPiston1.set_value(true);  // Deploy the piston
+    //    GrabberPiston2.set_value(true);
+    //    isGrabberExtended = true;   // Indicate it's set
+    //    waitingOnReleaseR2 = true;  // Flag to wait for button release
+    //  }
+    //  else if( (master.get_digital(pros::E_CONTROLLER_DIGITAL_R2) == 1) && (isGrabberExtended == true) && (waitingOnReleaseR2 == false) ) {
+    //    GrabberPiston1.set_value(false);  // Retract the piston
+    //    GrabberPiston2.set_value(false);
+    //    isGrabberExtended = false;  // Indicate it's not set
+    //    waitingOnReleaseR2 = true;  // Flag to wait for button release
+    //  }
+    //  else if((master.get_digital(pros::E_CONTROLLER_DIGITAL_R2) == 0 )) {
+    //    waitingOnReleaseR2 = false;  // Flag that button has been released, so next press will toggle to the opposite state
+    //  }
 
      pros::delay(20);
 
